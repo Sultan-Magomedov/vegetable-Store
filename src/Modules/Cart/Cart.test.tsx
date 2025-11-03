c29240c6f04034c4988fa8064eb6f13adadec4a6
Magomedov Sultan <sultanatus008@mail.ru>
2025-11-03 14:46:06 +0300

    first

--- src/Modules/Cart/Cart.test.tsx
+++ src/Modules/Cart/Cart.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it} from "vitest";
 import { screen, renderWithRedux } from "../../../test-utils";
 import Cart from "./Cart";
 import "@testing-library/jest-dom";
